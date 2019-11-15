ee0bce919d0d62b2a4652eedd9b4e93b804b542f
Logan Kilpatrick <lok026@g.harvard.edu>
2019-11-15 15:07:42 -0500

    Update POMDPSimulators.jl
    Per a conversation with @ebalaban yesterday, I think these exports should be removed.

--- src/POMDPSimulators.jl
+++ src/POMDPSimulators.jl
@@ -17,10 +17,6 @@ include("rollout.jl")
 
 export
     SimHistory,
-    POMDPHistory,
-    MDPHistory,
-    AbstractPOMDPHistory,
-    AbstractMDPHistory,
     HistoryIterator,
     eachstep,
     state_hist,
